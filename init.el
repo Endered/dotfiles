2aa55d5806a2269879e23c39c0e8b2b6d959f57f
Endered <yy56ga10ve@gmail.com>
2022-04-09 22:21:39 +0900

    remove unneeded declara

--- init.el
+++ init.el
@@ -166,7 +166,6 @@
 		    indent-tabs-mode nil
 		    company-minimum-prefix-length 2
 		    lsp-enable-indentation nil
-		    lsp-clients-clangd-executable "clangd"
 		    read-process-output-max (* 1024 1024)
 		    lsp-idle-delay 2.000))))
 
