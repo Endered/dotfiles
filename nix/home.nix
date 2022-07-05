c5e2cbb01ddb63c99502a73a98d7b9a2500ad54b
Endered <yy56ga10ve@gmail.com>
2022-07-06 02:19:18 +0900

    use wezterm or i3-sensible-terminal

---
+++ nix/home.nix
@@ -0,0 +1,113 @@
+{config, pkgs, ...}:
+{
+
+  nixpkgs.config.allowUnfree = true;
+
+  home.username = builtins.getEnv "USER";
+  home.homeDirectory = builtins.getEnv "HOME";
+
+
+  home.packages = with pkgs; [
+    gcc
+    gnumake
+    libtool
+    cmake
+    glib
+    slack
+    neovim
+    python310
+    xsel
+    xclip
+    evince
+    sshfs
+    gnome.gnome-screenshot
+    gnome3.dconf-editor
+    openssl
+    pavucontrol
+    discord
+    zathura
+    gzip
+    dunst
+    libnotify
+    screen
+    espeak-ng
+    tigervnc
+    baobab
+    tmux
+    wezterm
+  ];
+
+  # EDITOR
+  programs.emacs = {
+    enable = true;
+    extraPackages = epkgs: [
+      epkgs.vterm
+    ];
+  };
+
+  xresources.properties = {
+    "Xft.dpi" = 120;
+  };
+
+  # bash
+  programs.bash = {
+    enable = true;
+    sessionVariables = {
+    };
+    initExtra = ''
+               if [ -e ~/dotfiles/bashrc ]; then
+                 source ~/dotfiles/bashrc
+               fi
+               
+               # disable middle mouse pointer
+               function disable_embedded(){
+                  for id in $(xinput | grep TPPS | grep -oP '(?<=id=)\d*')
+                  do
+                      xinput disable $id
+                  done
+               }
+               function enable_embedded(){
+                  for id in $(xinput | grep TPPS | grep -oP '(?<=id=)\d*')
+                  do
+                      xinput enable $id
+                  done
+               }
+    '';
+  };
+
+  # git
+  programs.git = {
+    enable = true;
+    userName = "Endered";
+    userEmail = "yy56ga10ve@gmail.com";
+  };
+
+  home.file = {
+    ".config/i3/config" = {
+      source = ~/dotfiles/config/i3/config;
+    };
+    ".config/i3status/config" = {
+      source = ~/dotfiles/config/i3status/config;
+    };
+    ".vimrc" = {
+      source = ~/dotfiles/vimrc;
+    };
+    ".config/nvim/init.vim" = {
+      source = ~/dotfiles/init.vim;
+    };
+    ".config/nvim/dein.toml" = {
+      source = ~/dotfiles/dein.toml;
+    };
+    ".config/nvim/dein_lazy.toml" = {
+      source = ~/dotfiles/dein_lazy.toml;
+    };
+    ".emacs.d/init.el" = {
+      source = ~/dotfiles/init.el;
+    };
+    ".wezterm.lua" = {
+      source = ~/dotfiles/wezterm.lua;
+    };
+  };
+
+  programs.home-manager.enable = true;
+}
