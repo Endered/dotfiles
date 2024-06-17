d9a06cbfaf4fe9c400d330badb58d591aea45b3b
Endered <yy56ga10ve@gmail.com>
2024-06-18 03:21:28 +0900

    Nix: changeable wezterm font size

--- nix/environments/wezterm.nix
+++ nix/environments/wezterm.nix
@@ -5,6 +5,12 @@ let
     url = "https://github.com/wez/wezterm/releases/download/20240203-110809-5046fc22/WezTerm-20240203-110809-5046fc22-Ubuntu20.04.AppImage";
     hash = "sha256-NAEKBwdtInLE1PlLXg2uYIpnlZno1ylEYyP4j5VsYPA=";
   };
+  pure-setting-file = ~/dotfiles/wezterm.lua;
+  font-size = toString cfg.font-size;
+  setting-file = pkgs.runCommand "modyfied-wezterm.lua" {} ''
+               cat ${pure-setting-file} |
+                 sed -e 's/font_size.*/font_size = ${font-size},/' > $out
+  '';
 in
 {
   options.my-settings.wezterm = {
@@ -15,6 +21,10 @@ in
     use-appimage = lib.mkOption {
       default = false;
       type = lib.types.bool;
+    };
+    font-size = lib.mkOption {
+      default = 9;
+      type = lib.types.number;
     };
   };
 
@@ -28,7 +38,7 @@ in
           executable = true;
         };
         ".wezterm.lua" = {
-          source = ~/dotfiles/wezterm.lua;
+          source = setting-file;
         };
       };
     };
